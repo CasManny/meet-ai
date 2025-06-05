6b42f5784e00a362a1f8927aa1432578f7008035
CasManny <chukwusabastine2018@gmail.com>
2025-06-05 23:55:04 +0100

    04: Authentication setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,16 @@
 import { betterAuth } from "better-auth";
-import { Pool } from "pg";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db"; // your drizzle instance
+import * as schema from "@/db/schema"
 
 export const auth = betterAuth({
-  database: new Pool({
-    // connection options
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "mysql", "sqlite"
+    schema: {
+      ...schema
+    }
   }),
 });
