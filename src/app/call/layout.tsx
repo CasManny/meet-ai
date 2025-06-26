f4c5b7e3767a76b0e90b1aa9e0adca2b2e9e509c
CasManny <chukwusabastine2018@gmail.com>
2025-06-26 22:41:36 +0100

    22: Video Call

---
+++ src/app/call/layout.tsx
@@ -0,0 +1,7 @@
+import React, { PropsWithChildren } from "react";
+
+const CallLayout = ({ children }: PropsWithChildren) => {
+  return <div className="h-screen bg-black">{children}</div>;
+};
+
+export default CallLayout;
